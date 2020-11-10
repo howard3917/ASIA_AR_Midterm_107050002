3343ce1cc216098a04f5e6f316fb9e54e159206d
howard <howard3917@gmail.com>
2020-11-10 23:32:01 +0800

    FINISH

--- AR_Midterm_107050002 2.0/Assets/scripts/GameManager.cs
+++ AR_Midterm_107050002 2.0/Assets/scripts/GameManager.cs
@@ -14,6 +14,10 @@ public class GameManager : MonoBehaviour
     public float turn = 1.5f;
     [Header("縮放"), Range(0f, 5f)]
     public float size = 0.3f;
+    [Header("女動畫元件")]
+    public Animator anifemale;
+    [Header("男動畫元件")]
+    public Animator animale;
 
     private void Start()
     {
@@ -25,16 +29,34 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         print("更新事件");
-        print ("joystick Vertical");
+        print(joystick.Vertical);
 
         male.Rotate(0, joystick.Horizontal* turn, 0);
         female.Rotate(0, joystick.Horizontal * turn, 0);
 
-        male.localScale =new Vector3(1, 1, 1) * joystick.Vertical;
+        male.localScale +=new Vector3(1, 1, 1) * joystick.Vertical;
 
-        female.localScale = new Vector3(1, 1, 1) * joystick.Vertical;
+        male.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(male.localScale.x, 0.5f, 3.5f);
+
+        female.localScale += new Vector3(1, 1, 1) * joystick.Vertical;
+
+        female.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(female.localScale.x, 0.5f, 3.5f);
+
+
+
+
+
+
     }
+    public void PlayAnimation(string aniName)
+    {
+        print(aniName);
+        animale.SetTrigger(aniName);
+        anifemale.SetTrigger(aniName);
 
+
+
+        }
 
 
 }
